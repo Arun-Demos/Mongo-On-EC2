6cae8537224abf3e2a5bab51bfcbea55b29da318
Arun Kumar <87801852+arunrana1214@users.noreply.github.com>
2025-09-15 15:41:55 +0800

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,13 +1,4 @@
-output "aws_account_id" { value = var.aws_account_id }
-output "aws_region"     { value = var.aws_region }
-
 output "mongo_private_ip" { value = aws_instance.mongo.private_ip }
-output "security_group_id" { value = aws_security_group.mongo.id }
-
-output "backup_bucket" { value = aws_s3_bucket.mongo_backups.bucket }
-output "backup_prefix" { value = var.backup_prefix }
-
-output "admin_password_parameter" { value = aws_ssm_parameter.mongo_admin.name }
-output "connection_string_note" {
-  value = "Fetch admin password from SSM ${aws_ssm_parameter.mongo_admin.name}; connect to mongodb://admin:<PASS>@${aws_instance.mongo.private_ip}:27017/admin?authSource=admin"
-}
+output "mongo_public_ip"  { value = aws_instance.mongo.public_ip }
+output "admin_password_param" { value = aws_ssm_parameter.mongo_admin.name }
+output "backup_bucket"    { value = aws_s3_bucket.mongo_backups.bucket }
