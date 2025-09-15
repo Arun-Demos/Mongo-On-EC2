d96071ee4b464df4f3c0257c98d64fffdd96f765
Arun Kumar <87801852+arunrana1214@users.noreply.github.com>
2025-09-15 15:36:49 +0800

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = var.aws_region
 
-  # If ASSUME ROLE ARN is provided, use it (cross-account).
   assume_role {
     role_arn     = var.assume_role_arn != "" ? var.assume_role_arn : null
     session_name = "terraform-mongo-ec2"
