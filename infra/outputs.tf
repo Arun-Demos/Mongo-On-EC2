d0d6804d47540fcba481f0eb918420624ee54114
Arun Kumar <87801852+arunrana1214@users.noreply.github.com>
2025-09-19 23:04:26 +0800

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,11 +1,6 @@
-output "aws_account_id"      { value = var.aws_account_id }
-output "aws_region"          { value = var.aws_region }
-output "mongo_private_ip"    { value = aws_instance.mongo.private_ip }
-output "mongo_public_ip"     { value = aws_instance.mongo.public_ip }
-output "security_group_id"   { value = aws_security_group.mongo.id }
-output "admin_password_param"{ value = aws_ssm_parameter.mongo_admin.name }
-output "backup_bucket"       { value = aws_s3_bucket.mongo_backups.bucket }
-output "backup_prefix"       { value = var.backup_prefix }
-output "connection_string_note" {
-  value = "Fetch admin password from SSM ${aws_ssm_parameter.mongo_admin.name}; connect: mongodb://admin:<PASS>@${aws_instance.mongo.private_ip}:27017/admin?authSource=admin"
-}
+output "instance_id"        { value = aws_instance.mongo.id }
+output "public_ip"          { value = aws_instance.mongo.public_ip }
+output "private_ip"         { value = aws_instance.mongo.private_ip }
+output "security_group_id"  { value = aws_security_group.mongo.id }
+output "admin_password_param" { value = aws_ssm_parameter.mongo_admin.name }
+output "backup_bucket"      { value = var.backup_bucket_name }
